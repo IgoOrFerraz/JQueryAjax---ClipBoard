d439679dd4f84d687ffd7f7354b36014ba02bf47
IgoOrFerraz <igoorferraz23@gmail.com>
2021-01-10 15:08:47 -0300

    AssyncRequest

--- functions.js
+++ functions.js
@@ -5,12 +5,37 @@ $(document).ready(()=>{
         success: (result)=>{
             let id=0
             result.forEach(()=>{
-                $('#btn-select').append("<option value='"+id+"'>"+result[id]['category']+"</option>")
+                $('#btn-select').append("<option value='"+result[id]['category']+"'>"+result[id]['category']+"</option>")
                 id++
             })
         },
         error: ()=>{
-            console.log("Error")
+            console.log("error select category")
         } 
     })
+    $('.btn-register').on('click', ()=>{
+        console.log("BtnRegis")
+        let reg = new Register({
+            'name': $('.reg-name').val(),
+            'date': $('.reg-date').val(),
+            'initial': $('.reg-initial').val(),
+            'final': $('.reg-final').val(),
+            'specification': $('.reg-spe').val(),
+            'requirements': $('.reg-req').val(),
+            'category': $('.reg-cat').val()
+        })
+        console.log(reg)
+    })
 })
+
+class Register{
+    constructor(obj){
+        this.name = obj['name']
+        this.date = obj['date']
+        this.initial = obj['initial']
+        this.final = obj['final']
+        this.specification = obj['specification']
+        this.requirements = obj['requirements']
+        this.category = obj['category']
+    }
+}
