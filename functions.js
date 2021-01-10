c4fa50ae7710f9f059893aa8fbe8d23395fba2d3
IgoOrFerraz <igoorferraz23@gmail.com>
2021-01-10 15:42:48 -0300

    Introduce of AutenticReg

--- functions.js
+++ functions.js
@@ -14,8 +14,12 @@ $(document).ready(()=>{
         } 
     })
     $('.btn-register').on('click', ()=>{
-        console.log("BtnRegis")
+        
+        let atualdate = new Date()
+        atualdate = atualdate.getFullYear()+'-'+(atualdate.getMonth()+1)+'-'+atualdate.getDate()
+        
         let reg = new Register({
+            'today': atualdate,
             'name': $('.reg-name').val(),
             'date': $('.reg-date').val(),
             'initial': $('.reg-initial').val(),
@@ -30,6 +34,7 @@ $(document).ready(()=>{
 
 class Register{
     constructor(obj){
+        this.atualdate = obj['today']
         this.name = obj['name']
         this.date = obj['date']
         this.initial = obj['initial']
@@ -37,5 +42,31 @@ class Register{
         this.specification = obj['specification']
         this.requirements = obj['requirements']
         this.category = obj['category']
+    }
+
+    autentic(){
+        if(
+            this.name != '' || this.name != undefined &&
+            this.date != '' || this.date != undefined &&
+            this.initial != '' || this.initial != undefined &&
+            this.final != '' || this.final != undefined &&
+            this.specification != '' || this.specification != undefined &&
+            this.requirements != '' || this.requirements != undefined &&
+            this.category != '' || this.category != undefined
+        ){
+            if(
+                this.final > this.initial &&
+                autenticdate(this.atualdate, this.date)
+            ){
+                return true
+            } else{
+                return false
+            }
+        } else{
+            return false
+        }
+    }
+    autenticdate(atualdate, date){
+        
     }
 }
